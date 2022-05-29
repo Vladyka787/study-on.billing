c3c887b1781ffdead69c16b331ae2f670cc80cf3
Vlad <vladyka89525968448@gmail.com>
2022-05-29 18:15:46 +0300

    Начало 5 урока. Создана бд и сущность User. Сгенерированы CRUD для юзер и добавлена форма регистрации. Созданы миграции. Добавлена фикстура, создающая 2 пользователя с Хешированными паролями.

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $userOne = new User();
+        $userOne->setEmail('userOne@mail.ru');
+
+        $passwordHasherFactory = new PasswordHasherFactory([
+            PasswordAuthenticatedUserInterface::class => ['algorithm' => 'auto'],
+        ]);
+        $hashPassword = new UserPasswordHasher($passwordHasherFactory);
+        $hash = $hashPassword->hashPassword($userOne, 'Password');
+        $userOne->setPassword($hash);
+
+        $userOne->setRoles(['ROLE_USER']);
+
+
+        $userTwo = new User();
+        $userTwo->setEmail('userTwo@mail.ru');
+
+        $hash = $hashPassword->hashPassword($userTwo, 'SuperPassword');
+        $userTwo->setPassword($hash);
+
+        $userTwo->setRoles(['ROLE_SUPER_ADMIN']);
+
+        $manager->persist($userOne);
+        $manager->persist($userTwo);
+
+
+        $manager->flush();
+    }
+}
